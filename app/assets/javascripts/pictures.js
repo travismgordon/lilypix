740f9134c99b3da867e38f5a5fb5e4771c06ef9f
Travis Gordon <travismgordon@gmail.com>
2015-02-12 02:50:54 -0500

    BOOM! aww yiss.

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,4 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-
+// Touch device compatability, brah
+$(function(){
+  // See if this is a touch device
+  if ('ontouchstart' in window)
+  {
+    // Set the correct body class
+    $('body').removeClass('no-touch').addClass('touch');
+    
+    // Add the touch toggle to show text
+    $('div.boxInner img').click(function(){
+      $(this).closest('.boxInner').toggleClass('touchFocus');
+    });
+  }
+});
