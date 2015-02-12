f1b0a216eaef23d61655b48f9d2a8e31e0ab8537
Travis Gordon <travismgordon@gmail.com>
2015-02-12 14:43:51 -0500

    comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -64,6 +64,10 @@ class CommentsController < ApplicationController
     end
   end
 
+
+
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
@@ -73,6 +77,7 @@ class CommentsController < ApplicationController
     def set_picture
       @picture = Picture.find(params[:picture_id])
     end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
